4b07e56ba6221e904ef00e26bae578ce7a75256f
Caleb Lockwood <caleb.lockwood626@gmail.com>
2023-05-25 14:40:39 -0600

    various minor seed updates aligning seeds from previous version of project to this version's models

--- seeds/commentSeeds.js
+++ seeds/commentSeeds.js
@@ -5,21 +5,21 @@ const commentData = [
         user_id: 1,
         post_id: 1,
         timestamp: "05-13-2023",
-        comment_text: "Nooooooooooooo! MaNcHeStEr CiTy is only good because of oil--they can't be allowed to win!"
+        comment_content: "Nooooooooooooo! MaNcHeStEr CiTy is only good because of oil--they can't be allowed to win!"
     },
 
     {
         user_id: 2,
         post_id: 3,
         timestamp: "06-20-2023",
-        comment_text: "yeah! Ive seen the Office 34 times."
+        comment_content: "yeah! Ive seen the Office 34 times."
     },
 
     {
         user_id: 3,
         post_id: 2,
         timestamp: "07-08-2023",
-        comment_text: "It's true. Elon Musk isnt a genius, he's just like every other capitalist in the Oligarch class."
+        comment_content: "It's true. Elon Musk isnt a genius, he's just like every other capitalist in the Oligarch class."
     }
 ];
 
--- seeds/userSeeds.js
+++ seeds/userSeeds.js
@@ -3,19 +3,16 @@ const { User } = require("../models");
 const userData = [
     {
         username: "Kev1nEatz",
-        email: "KMalone@dundermifflin.com",
         password: "Ke11venby7"
     },
 
     {
         username: "DSchrute",
-        email: "DSchrute@@dundermifflin.com",
         password: "iMissAngela!"
     },
 
     {
         username: "Jimmy-Samtanko",
-        email: "JHalpert@dundermifflin.com",
         password: "BearsBeatsBattlestarGallacticga!"
     }
 ];
