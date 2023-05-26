2e7180a58a90572dd8887319a0a9ab9459d9e001
Caleb Lockwood <caleb.lockwood626@gmail.com>
2023-05-25 20:43:37 -0600

    api index updated

---
+++ controllers/api/index.js
@@ -0,0 +1,9 @@
+const userRoutes = require('./userRoutes');
+const postRoutes = require('./postRoutes');
+const commentRoutes = require('./commentRoutes');
+
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
+
+module.exports = router;
