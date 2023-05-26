c659cde21664e45ba3c023a8214bc84ce43c3f87
Caleb Lockwood <caleb.lockwood626@gmail.com>
2023-05-25 20:41:06 -0600

    commentroutes updated

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -55,7 +55,63 @@ router.get('/:id', async (req, res) => {
     }
 });
 
+// create new comment
+router.post('/', async (req, res) => {
+    console.log('user id:');
+    console.log(req.session.user_id)
+    try {
+   const newComment = await Comment.create({
+    ...req.body,
+    post_id: req.body.post_id,
+    user_id: req.session.user_id,
+    timestamp: Date.now()
+    });
+        if(!newComment) {
+            res.status(404).json({ message: 'Please enter comment info'});
+            return;
+        }
+        res.status(200).json(newComment);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
+// update a comment by id
+router.put('/:id', async (req, res) => {
+    try {
+        const commentToUpdate = await Comment.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+        if(!commentToUpdate[0]) {
+            res.status(400).json({ message: 'No comment with this ID!' });
+            return;
+        }
+        res.status(200).json(commentToUpdate); 
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// delete a comment by id
+router.delete('/:id', async (req, res) => {
+    try {
+        const commentToDelete = await Comment.findByPk(req.params.id) 
+        Comment.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });    
+        if(!commentToDelete) {
+            res.status(400).json({ message: 'No comment with this ID!' });
+            return;
+        }
+        res.status(200).json(commentToDelete); 
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
 module.exports = router;
